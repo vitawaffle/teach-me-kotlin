ce2781a1ff345a59462921c3e79d48975f317175
Vitaly Lobatsevich <vitaly.lobatsevich@easybrain.com>
2023-01-09 20:09:19 +0100

    Removed unused user entity, added new redirection to nginx config

--- src/main/kotlin/by/vitalylobatsevich/teachme/repository/UserRepository.kt
+++
@@ -1,7 +0,0 @@
-package by.vitalylobatsevich.teachme.repository
-
-import by.vitalylobatsevich.teachme.entity.User
-import org.springframework.stereotype.Repository
-
-@Repository
-interface UserRepository : LongIdEntityRepository<User>
